c7ca9694127d11e845ed046e7949555694a297b3
Marchys <barros.marcel92@gmail.com>
2015-03-03 09:08:25 +0100

    Base Fragments 4

---
+++ Fragments_P4/app/src/main/java/com/example/marcelba/fragments_p4/Frag_2.java
@@ -0,0 +1,30 @@
+package com.example.marcelba.fragments_p4;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Frag_2 extends Fragment {
+
+
+    public Frag_2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_2, container, false);
+    }
+
+
+}
